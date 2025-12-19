a63210e6c198aa131edf4db49f73130422c47a50
07-khanh <khanh.npn07@gmail.com>
2025-12-19 10:24:57 +0700

    Update data file path to relative location

--- src/source.py
+++ src/source.py
@@ -18,7 +18,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-data = pd.read_csv('real_estate.csv')
+data = pd.read_csv('../data/real_estate.csv')
 
 print("\n<Dataset's information>:")
 print(f"1. Size: {data.shape}")
